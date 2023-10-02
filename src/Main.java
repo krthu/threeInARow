62b6a4b942e6fae2be2592ebfd4cd1c7fdb88078
KT <thun.kristian@gmail.com>
2023-10-02 09:06:38 +0200

    Added psuedo code in main and created player class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Introduce the game
+
+        // Ask for players name
+        //Create the players
+        // Add to Player - array
+
+
+        // Create the GameBoard
+
+        // Loop until players are done with game
+            //Loop over players until someone wins Or Scoreboard is full .
+                //Ask player for input
+                //Save the input
+                //Check for winner
+                //if weHaveAWinner
+                    //Player gets score
+                    // breaks loop
+                //else if Game board is full
+                    // Print that it is a tie game
+                    // Break loop
+            //Ask for another game
     }
 }
---
+++ src/Player.java
@@ -0,0 +1,15 @@
+public class Player {
+    private String name;
+    private int score;
+
+    private String sign;
+
+    public Player(String name, String sign){
+        this.name = name;
+        this.sign = sign;
+        score = 0;
+    }
+
+
+
+}
